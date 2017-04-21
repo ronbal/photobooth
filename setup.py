acdcb72c2ef96202e73c2517af1f6f7e6867069f
Ronny Schönfeld <ronny@ronbal.de>
2017-04-21 20:48:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import subprocess
 from subprocess import STDOUT, check_call
 import os
-import time
+from time import sleep
 WorkingDir = os.getcwd()
 print("Starting update")
 check_call(['sudo', 'apt-get', 'update', '-y'])
