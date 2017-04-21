a8bab17d08df46bc5b3ada0238374460776f615a
Ronny Schönfeld <ronny@ronbal.de>
2017-04-21 11:05:55 +0000

    new setup routine

---
+++ setup.py
@@ -0,0 +1,26 @@
+import subprocess
+from subprocess import STDOUT, check_call
+import os
+WorkingDir = os.getcwd()
+print("Starting update")
+check_call(['sudo', 'apt-get', 'update', '-y'])
+print("Starting upgrade")
+check_call(['sudo', 'apt-get', 'upgrade', '-y'])
+print("installing apache2")
+check_call(['sudo', 'apt-get', 'install', 'apache2', '-y'])
+print("installing php5")
+check_call(['sudo', 'apt-get', 'install', 'php5', '-y'])
+print("installing imagemagick")
+check_call(['sudo', 'apt-get', 'install', 'imagemagick', '-y'])
+print("make directory images")
+subprocess.call('sudo mkdir /var/www/html/images', shell=True)
+print("make directory thumbs for thumbnmails")
+subprocess.call('sudo mkdir /var/www/html/thumbs', shell=True)
+print("copy webserver files")
+subprocess.call('sudo cp -R /home/pi/photobooh/webserver/* /home/var/www/html', shell=True)
+print("delete the old index.html")
+subprocess.call('sudo rm -r /var/www/html/index.html', shell=True)
+print("your PHOTOBOOTH is ready")
+print("please start the photobooth with:")
+print("python3 /home/pi/photobooth/photobooth.py")
+
